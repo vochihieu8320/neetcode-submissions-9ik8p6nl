049246ccbd12bab790daaa7cc543a9e07c9e7856
vochihieu8320 <51293321+vochihieu8320@users.noreply.github.com>
2026-05-31 12:01:10 +0700

    Add: is-palindrome - submission-10

---
+++ Data Structures & Algorithms/is-palindrome/submission-10.py
@@ -0,0 +1,21 @@
+import re
+import math
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        string_splited = s.split(" ")
+        normalize_string = ""
+        for i in range(len(string_splited)):
+            normalize_string += re.sub(r'[^a-zA-z0-9]', '', string_splited[i].lower())
+
+        mid = math.ceil( len(normalize_string) / 2 )
+        print("")
+        print("mid", normalize_string)
+        for i in range(0, mid):
+            if normalize_string[i] != normalize_string[len(normalize_string) - 1 - i]:
+                return False
+        
+        return True
+      
+        
+        
